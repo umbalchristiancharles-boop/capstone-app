ad908ed72b64a33691179093e1d8dd1e769280be
Christian Charles S. Umbal <umbal.christiancharles@ncst.edu.ph>
2026-09-13 22:23:48 +0800

    updated chaaaaa

--- resources/js/components/SupplierPanel.vue
+++ resources/js/components/SupplierPanel.vue
@@ -1578,7 +1578,7 @@ async function confirmLogout() {
   try { localStorage.clear(); sessionStorage.clear(); } catch (e) {}
   setTimeout(() => {
     try { localStorage.clear(); sessionStorage.clear(); } catch (e) {}
-    try { window.location.replace('/staff-landing') } catch (e) { /* ignore */ }
+    try { window.location.replace('/admin-login') } catch (e) { /* ignore */ }
   }, 600)
 }
 
